ec7c104e28631539e9206b5d0cf2c8ce3d6549e2
Mastayxz <gustibagus826@gmail.com>
2025-08-28 09:02:15 +0800

    admin-page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
